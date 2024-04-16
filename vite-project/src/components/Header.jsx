c4b607053872d6a2868d06dc398af174a82e0f46
Fnami51 <fnm5160@gmail.com>
2024-04-16 20:41:28 +0300

    v3.2.1 - Task №1

--- vite-project/src/components/Header.jsx
+++ vite-project/src/components/Header.jsx
@@ -1,6 +1,12 @@
 import '../App.css'
+import { useState } from 'react';
 
 function Header() {
+    const [isOpen, setIsOpen] = useState(false)
+    const openMenu = () => {
+        setIsOpen((prevState) => !prevState);
+    };
+
     return <header className="header">
     <div className="container">
         <div className="header__block">
@@ -12,8 +18,9 @@ function Header() {
             </div>
             <nav className="header__nav">
                 <button className="header__btn-main-new _hover01" id="btnMainNew"><a href="#popNewCard">Создать новую задачу</a></button>
-                <a href="#user-set-target" className="header__user _hover02">Ivan Ivanov</a>
-                <div className="header__pop-user-set pop-user-set" id="user-set-target">
+                <a onClick={openMenu} className="header__user _hover02">Ivan Ivanov</a>
+            
+                <div style={isOpen ? {display: "block"} : {display: "none"}} className="header__pop-user-set pop-user-set" id="user-set-target">
                     <p className="pop-user-set__name">Ivan Ivanov</p>
                     <p className="pop-user-set__mail">ivan.ivanov@gmail.com</p>
                     <div className="pop-user-set__theme">
