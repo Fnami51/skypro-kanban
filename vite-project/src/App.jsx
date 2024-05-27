9f9a9ddcec2ce959520ceef986e2f1c74d5757b4
Fnami51 <fnm5160@gmail.com>
2024-05-27 22:58:48 +0300

    v3.5.3 - open function onCardAdd

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -9,21 +9,38 @@ import PopNewCard from './components/PopNewCard.jsx';
 import PrivateRoute from './components/PrivateRoute.jsx';
 import NotFound from './pages/NotFoundPage/NotFound.jsx';
 import { UserProvider} from './components/UserContext.jsx';
+import { useState } from 'react'
+import { cardList } from './components/data.js';
+
 
 
 function App() {
 	console.log(localStorage.getItem("token"))
   const isAuth = Boolean(localStorage.getItem("token"));
   console.log(isAuth)
+
+  const [cards, setCards] = useState(cardList);
+
+	function onCardAdd() {
+        setCards((prevCards) => {
+            return [...prevCards, {
+                id: cardList.length + 1,
+                topic: "Web Design",
+                title: "Название задачи",
+                date: "30.10.23",
+                status: "Без статуса",
+            }]
+        });
+    }
   
   return (
 	<UserProvider>
 	<Routes>
 		<Route element={<PrivateRoute isAuth={isAuth} />}>
-			<Route path={'/'} element={<MainPage />}>
+			<Route path={'/'} element={<MainPage cards={cards}/>}>
 				<Route path={'/exit'} element={<Exit />}></Route>
 				<Route path={'/card/:cardId'} element={<PopBrowse />}></Route>
-				<Route path={'/newcard'} element={<PopNewCard />}></Route>
+				<Route path={'/newcard'} element={<PopNewCard onCardAdd={onCardAdd}/>}></Route>
 			</Route>
 		</Route>
 		<Route path={'/login'} element={<Login />}></Route>
--- vite-project/src/pages/MainPage/MainPage.jsx
+++ vite-project/src/pages/MainPage/MainPage.jsx
@@ -4,23 +4,9 @@ import { Wrapper } from '../style_pages/Login.styled.js'
 import Header from '../../components/Header.jsx'
 import Main from '../../components/Main.jsx'
 import Loading from '../../components/Loading.jsx'
-import { cardList } from '../../components/data.js'
 import { Outlet } from 'react-router-dom'
 
-function MainPage() {
-	const [cards, setCards] = useState(cardList);
-
-	/*function onCardAdd() {
-        setCards((prevCards) => {
-            return [...prevCards, {
-                id: cardList.length + 1,
-                topic: "Web Design",
-                title: "Название задачи",
-                date: "30.10.23",
-                status: "Без статуса",
-            }]
-        });
-    }*/
+function MainPage({cards}) {
 
 	const [isLoading, setIsLoading] = useState(true); //DEMO
 	function changeLoadingState() {setIsLoading(false)} //DEMO
